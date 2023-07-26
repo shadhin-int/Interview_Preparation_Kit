088b2eb3628d5086145b4f4afc8ad38b050cedfa
Shadhin <shahinur.rahman@upaybd.com>
2023-07-26 13:51:04 +0600

    solve 1 in day-1

---
+++ Day_1/plus_minus.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def plusMinus(arr):
+    arr_len = len(arr)
+    plus_count = 0
+    minus_count = 0
+    zero_count = 0
+    for i in arr:
+        if i > 0:
+            plus_count += 1
+        if i < 0:
+            minus_count += 1
+        if i == 0:
+            zero_count += 1
+    plus_ratio = plus_count/arr_len
+    minus_ratio = minus_count/arr_len
+    zero_ratio = zero_count/arr_len
+    print('%.6f' % plus_ratio, '\n%.6f' % minus_ratio, '\n%.6f' % zero_ratio)
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
