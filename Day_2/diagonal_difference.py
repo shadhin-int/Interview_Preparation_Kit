a5672eaa1234a47f08b0edccbb35760691d6b550
shadhin-int <shadhin.int@gmail.com>
2023-07-27 17:25:45 +0600

    updated

--- Day_2/diagonal_difference.py
+++ Day_2/diagonal_difference.py
@@ -1,11 +1,43 @@
-mt = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
-x_sum = 0
-y_sum = 0
-for i in range(len(mt)):
-    x_sum += mt[i][i]
+#!/bin/python3
 
-for i in range(len(mt)):
-    y_sum += mt[i][len(mt)-1-i]
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
 
 
-print(x_sum-y_sum)
+def diagonalDifference(arr):
+    x_sum = 0
+    y_sum = 0
+    for i in range(len(arr)):
+        x_sum += arr[i][i]
+
+    for i in range(len(arr)):
+        y_sum += arr[i][len(arr)-1-i]
+
+    return [(x_sum-y_sum) if x_sum > y_sum else (y_sum-x_sum)][0]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
