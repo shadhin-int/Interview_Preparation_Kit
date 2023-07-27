a713ca161c3d93586d1ecf4c7d1fc721ad2cb79d
shadhin-int <shadhin.int@gmail.com>
2023-07-27 14:54:12 +0600

    solved find lonely int in day 2

---
+++ Day_2/find_lonley_integer.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+
+def lonelyinteger(a):
+    l = []
+    for i in a:
+        if not i in l:
+            l.append(i)
+        else:
+            l.remove(i)
+
+    return l[0]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
